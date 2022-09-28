f1e2a000b05c9f218d596c92b7bac90119e0700d
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-09-28 15:15:04 -0600

    footer comp outline

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Card from '../components/Card';
-import Header from '../components/Header';
+
 import db from '../db.json';
 import { Post } from '../interfaces/blog/Post';
 
 const HomePage = () => {
 	return (
 		<div className="container mx-auto">
-			<Header />
 			<div className="mt-4 grid grid-cols-1  md:grid-cols-2">
 				{db.posts.map((post: Post) => (
 					<Card key={post.id} post={post} />
